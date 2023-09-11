1c47dd18d39cc23c4165b1cda58c7b01d928d1ef
Lubancy66 <144541020+Lubancy66@users.noreply.github.com>
2023-09-11 19:33:45 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,12 @@
+def isleapyear(year):
+  if (year / 4 == 0 and year / (100 != 0) or year / 400 == 0):
+    return True
+  else:
+    return False
+
+
+year = 2020
+if isleapyear(year):
+  print("2020 is a not a leapyear")
+else:
+  print("2020 is a not a leapyear")
